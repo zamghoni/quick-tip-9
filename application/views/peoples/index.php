600bb29ff14fca498c5cf3c0af8dedde33b84e8b
zamghoni <12171426@bsi.ac.id>
2020-05-14 21:40:34 +0700

    Membuat Fitur SEARCHING pada PAGINATION menggunakan CODEIGNITER 3 dan BOOTSTRAP 4

--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,7 +1,21 @@
 <div class="container">
+  <h3 class="mt-3">List of Peoples</h3>
+
+  <div class="row">
+    <div class="col-md-5">
+      <form class="" action="<?=base_url('peoples')?>" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Search Keyword.." name="keyword" autocomplete="off" autofocus>
+          <div class="input-group-append">
+            <input class="btn btn-primary" type="submit" name="submit">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
   <div class="row">
     <div class="col-md-12">
-      <h3 class="mt-3">List of Peoples</h3>
+      <h5>Result = <?=$total_rows;?></h5>
       <table class="table">
         <thead>
           <tr>
@@ -12,6 +26,15 @@
           </tr>
         </thead>
         <tbody>
+          <?php if (empty($peoples)): ?>
+            <tr>
+              <td colspan="4">
+                <div class="alert alert-danger" role="alert">
+                  Data not found!
+                </div>
+              </td>
+            </tr>
+          <?php endif; ?>
           <?php foreach ($peoples as $people): ?>
           <tr>
             <th><?=++$start?></th>
@@ -28,7 +51,7 @@
       </table>
 
       <?php echo $this->pagination->create_links(); ?>
-      
+
     </div>
   </div>
 </div>
