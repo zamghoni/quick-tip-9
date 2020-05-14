88ef5a4323ad5cb423860a2b48970876dcc815b3
zamghoni <12171426@bsi.ac.id>
2020-05-14 21:02:04 +0700

    Membuat WEBSITE menggunakan CODEIGNITER 3 dan BOOTSTRAP 4 (Bagian 3)

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -20,12 +20,32 @@
 
   <div class="row mt-3">
     <div class="col-md-6">
+      <form class="" action="" method="post">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Cari data mahasiswa.." name="keyword" >
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="submit">Cari</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-6">
       <h3>Daftar Mahasiswa</h3>
+      <?php if (empty ($mahasiswa)): ?>
+        <div class="alert alert-danger" role="alert">
+          Data mahasiswa tidak ditemukan          
+        </div>
+      <?php endif; ?>
       <ul class="list-group">
         <?php foreach ($mahasiswa as $mhs) : ?>
         <li class="list-group-item">
           <?=$mhs['nama']; ?>
           <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'];?>" class="badge badge-danger float-right" onclick="return confirm('yakin?')">hapus</a>
+          <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id'];?>" class="badge badge-success float-right">ubah</a>
+          <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-primary float-right">detail</a>
           </li>
       <?php endforeach; ?>
       </ul>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+    <!-- My css -->
+    <link rel="stylesheet" href="<?=base_url(); ?>assets/css/style.css">
+
     <title><?php echo $judul;?></title>
   </head>
   <body>
